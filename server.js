68d5afef3906d490af51a672b4f24df82d2197e7
Jayesh Patel <Jpatel4300@conestogac.on.ca>
2024-07-06 20:48:31 -0400

    created model and schema for user and comment collection

--- server.js
+++ server.js
@@ -27,13 +27,37 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+//product schema and model
 const productSchema = new mongoose.Schema({
   description: String,
   image: String,
   pricing: Number,
   shippingCost: Number
 });
+
 const Product = mongoose.model('Product', productSchema);
+
+//user schema and model
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  username: String,
+  purchaseHistory: Array,
+  shippingAddress: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+//comment schema and model
+const commentSchema = new mongoose.Schema({
+  product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  rating: Number,
+  images: [String],
+  text: String
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
