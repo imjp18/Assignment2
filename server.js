b1bd3064d6514cd0d03a23b31c63e9bbf443eabc
Jayesh Patel <Jpatel4300@conestogac.on.ca>
2024-07-06 20:30:15 -0400

    used multer to upload a product image and created product schema

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const multer = require('multer')
+const path = require('path')
 const PORT = 8080;
 
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use('/uploads', express.static('uploads'));
 
 const URI = '';
 
 mongoose.connect(URI)
   .then(() => console.log('connected to MongoDB'))
   .catch(err => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
+const productSchema = new mongoose.Schema({
+  description: String,
+  image: String,
+  pricing: Number,
+  shippingCost: Number
+});
+const Product = mongoose.model('Product', productSchema);
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
